4b5c1e3fb62c6b46e30de6b0bbf893335b5c8c1c
Pratik Patil <p.patil7@zensar.com>
2023-03-27 10:07:06 +0530

    adf

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ const app=express();
 const names=require('./userModel')
 const cors = require('cors');
 
-require('./db')
 app.use(cors())
 app.use(express.json())
 app.get('/',async(req,res)=>{
-  const data=await names.find();
-  res.send(data);
+
+  res.send('hello world');
 })
-app.post('/add',async(req,res)=>{
-    const {name}=req.body;
-    const newUser=new names({name});
-    const savedData=await newUser.save();
-    res.send(savedData);
-})
+// app.post('/add',async(req,res)=>{
+//     const {name}=req.body;
+//     const newUser=new names({name});
+//     const savedData=await newUser.save();
+//     res.send(savedData);
+// })
 const port=process.env.port||8000;
 app.listen(port,()=>{
     console.log('listening on poret 800')
